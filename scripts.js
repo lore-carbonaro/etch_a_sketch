396bbf1b0a7fd08688691f0df07b5dee4befc410
lore-carbonaro <lorenzocarbonaro3@gmail.com>
2025-03-12 19:19:57 +0100

    Ask user for grid size at button click

--- scripts.js
+++ scripts.js
@@ -1,5 +1,11 @@
+const body = document.querySelector("body")
+const button = document.createElement("button");
+button.classList.add("btn");
+body.appendChild(button);
+
+/* Create grid */
 function makeGrid(size) {
-    let screen = document.querySelector("#container");
+    let container = document.querySelector("#container");
     for (let i = 0; i < size; i++) {
       let column = document.createElement("div");
       column.classList.add("column");
@@ -11,14 +17,32 @@ function makeGrid(size) {
       container.appendChild(column);
     }
 }
-  
-makeGrid(16);
 
 
-const cell = document.querySelectorAll(".cell");
 
-for(let i = 0; i < cell.length; i++) {
-    cell[i].addEventListener("mouseenter", () => {
-        cell[i].style.backgroundColor = "black";
-    });
+/* Clear container */
+function clearBox(elementID)
+{
+    document.querySelector(elementID).innerHTML = "";
 }
+
+
+
+/* User input to create grid */
+button.addEventListener("click", () => {
+    clearBox("#container");
+    let size = prompt("How big should the grid be?");
+
+    if (size > 100) {
+        alert("Too big, please choose a smaller value");
+    } else {
+        makeGrid(size);
+        const cell = document.querySelectorAll(".cell");
+
+        for(let i = 0; i < cell.length; i++) {
+            cell[i].addEventListener("mouseenter", () => {
+            cell[i].style.backgroundColor = "black";
+            });
+        }
+    }
+})
